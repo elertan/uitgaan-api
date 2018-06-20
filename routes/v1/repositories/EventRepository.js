d34d522f363bbf2d147bd3d4fa5b85bb6fdef8a7
elertan <denkievits@gmail.com>
2018-06-20 16:06:48 +0200

    API FIX NIGGER

--- routes/v1/repositories/EventRepository.js
+++ routes/v1/repositories/EventRepository.js
@@ -7,33 +7,59 @@ export default class EventRepository extends BaseRepository {
 
     static async getEvents() {
         try {
+            const events = await Event.find({}).sort({
+                till: 1
+            }).exec();
             const users = await UserRepository.getAll();
-            const events = await Event.find({}).sort({ till: 1 }).exec();
-            const eventsWithUsers = events.map(event => Object.assign(event, { user: users.find(user => user.username === event.username) }));
+            const eventsWithUsers = events.map(event => { 
+                const user = event.username ? users.find(user => {
+                    user.avatar = null;
+                    return user.username === event.username; 
+                }) : null;
+
+                const docEvent = event._doc;
+                const data = Object.assign({}, docEvent, {
+                    user
+                });
+
+                return data;
+            });
             return eventsWithUsers;
-        } catch (err) { }
-        return events;
+        } catch (err) {
+            return;
+        }
     }
 
-    static async addEvent(data,username) {
-        const event = await Event.findOne({ name: data.name }).exec();
+    static async addEvent(data, username) {
+        const event = await Event.findOne({
+            name: data.name
+        }).exec();
         if (event) {
             throw new Error('Dit event bestaat al.');
         }
-        const eventToCreate = new Event(Object.assign(data,{username}));
+        const eventToCreate = new Event(Object.assign(data, {
+            username
+        }));
         await eventToCreate.save();
         return eventToCreate;
     }
 
     static async filterEvents(name) {
-        const events = await Event.find({ "name": { "$regex": name, "$options": "i" }}).exec();
+        const events = await Event.find({
+            "name": {
+                "$regex": name,
+                "$options": "i"
+            }
+        }).exec();
         return eventsWithUsers;
         if (!events) {
             throw new Error('Geen evenementen gevonden.');
         }
         const users = await UserRepository.getAll();
-        const eventsWithUsers = events.map(event => Object.assign(event, { user: users.find(user => user.username === event.username) }));
-        
+        const eventsWithUsers = events.map(event => Object.assign(event, {
+            user: users.find(user => user.username === event.username)
+        }));
+
         return eventsWithUsers;
     }
 }
